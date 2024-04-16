468c668a8f158c304e7e31085bb2518b72358d23
FabiansDock <fabian20122001@gmail.com>
2024-04-16 20:11:26 +0530

    Added data to the main ui page and updated the request to include category

--- frontend/tmg/lib/main.dart
+++ frontend/tmg/lib/main.dart
@@ -5,6 +5,8 @@ import 'package:http/http.dart' as http;
 import 'package:image_picker/image_picker.dart';
 import 'package:another_flutter_splash_screen/another_flutter_splash_screen.dart';
 
+enum PlantCategory { grapes, mango, tomato }
+
 void main() {
   runApp(const MyApp());
 }
@@ -46,6 +48,7 @@ class ImageUploadPage extends StatefulWidget {
 class _ImageUploadPageState extends State<ImageUploadPage> {
   File? _imageFile;
   String? _responseText;
+  PlantCategory? _plant;
 
   final ImagePicker _picker = ImagePicker();
   bool _isAnimated = false;
@@ -66,6 +69,12 @@ class _ImageUploadPageState extends State<ImageUploadPage> {
       });
       return;
     }
+    if (_plant == null) {
+      setState(() {
+        _responseText = "Please select plant category.";
+      });
+      return;
+    }
 
     // API endpoint to upload the image
     String apiUrl = "http://10.0.2.2:5000/predict";
@@ -74,7 +83,8 @@ class _ImageUploadPageState extends State<ImageUploadPage> {
     var request = http.MultipartRequest('POST', Uri.parse(apiUrl));
     request.files
         .add(await http.MultipartFile.fromPath('image', _imageFile!.path));
-
+    request.fields['category'] = _plant!.index.toString();
+    
     // Send the request
     var streamedResponse = await request.send();
 
@@ -95,6 +105,10 @@ class _ImageUploadPageState extends State<ImageUploadPage> {
 
   @override
   Widget build(BuildContext context) {
+    var fontColor = Colors.purple[900];
+    const fontSize = 20.0;
+    const fontWeight = FontWeight.bold;
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('TMG Plant Disease Predictor'),
@@ -116,44 +130,149 @@ class _ImageUploadPageState extends State<ImageUploadPage> {
                     ? const Text('No image selected.')
                     : Image.file(_imageFile!),
               ),
-              const SizedBox(height: 20),
+              ListTile(
+                title: const Text('Grapes'),
+                leading: Radio<PlantCategory>(
+                  value: PlantCategory.grapes,
+                  groupValue: _plant,
+                  onChanged: (PlantCategory? value) {
+                    setState(() {
+                      _plant = value;
+                    });
+                  },
+                ),
+              ),
+              ListTile(
+                title: const Text('Mango'),
+                leading: Radio<PlantCategory>(
+                  value: PlantCategory.mango,
+                  groupValue: _plant,
+                  onChanged: (PlantCategory? value) {
+                    setState(() {
+                      _plant = value;
+                    });
+                  },
+                ),
+              ),
+              ListTile(
+                title: const Text('Tomato'),
+                leading: Radio<PlantCategory>(
+                  value: PlantCategory.tomato,
+                  groupValue: _plant,
+                  onChanged: (PlantCategory? value) {
+                    setState(() {
+                      _plant = value;
+                    });
+                  },
+                ),
+              ),
+              const SizedBox(height: 30),
               ElevatedButton(
                 onPressed: () {
                   _getImage(ImageSource.gallery);
+                  setState(() {
+                    _responseText = null;
+                  });
                 },
                 child: const Text('Pick Image from Gallery'),
               ),
+              const SizedBox(height: 20),
               ElevatedButton(
                 onPressed: () {
                   _getImage(ImageSource.camera);
+                  setState(() {
+                    _responseText = null;
+                  });
                 },
                 child: const Text('Take a Picture'),
               ),
               const SizedBox(height: 20),
               ElevatedButton(
                 onPressed: _uploadImage,
-                child: const Text('Upload Image and Send to API'),
+                child: const Text('Predict the Disease'),
               ),
               const SizedBox(height: 40),
               _responseText == null
                   ? Container()
-                  : _responseText!.length > 50 ? Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                            'Disease Name: \n\t${jsonDecode(_responseText!)[2]['Disease Name']}\n\n'),
-                        Text(
-                            'Causitive Agent: \n\t${jsonDecode(_responseText!)[0]['causitive_agent']}\n\n'),
-                        Text(
-                            'Scientific Name: \n\t${jsonDecode(_responseText!)[0]['scientific_name']}\n\n'),
-                        Text(
-                            'Symptoms: \n\t${jsonDecode(_responseText!)[0]['symptoms']}\n\n'),
-                        Text(
-                            'Treatment: \n\t${jsonDecode(_responseText!)[0]['treatment']}\n\n'),
-                        Text(
-                            'Probability: \n\t${jsonDecode(_responseText!)[1]['Probability']}\n\n'),
-                      ],
-                    ): Text(_responseText!),
+                  : _responseText!.length > 50
+                      ? Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              'Disease Name:',
+                              style: TextStyle(
+                                color: fontColor,
+                                fontWeight: fontWeight,
+                                fontSize: fontSize,
+                              ),
+                            ),
+                            Text('\t${jsonDecode(_responseText!)[2]['Disease Name']}\n\n',
+                              textAlign: TextAlign.left,
+                            ),
+                            Text(
+                                'Causitive Agent:',
+                                style: TextStyle(
+                                color: fontColor,
+                                fontWeight: fontWeight,
+                                fontSize: fontSize,
+                                ),
+                            ),
+                            Text('\t${jsonDecode(_responseText!)[0]['causitive_agent']}\n\n',
+                                textAlign: TextAlign.left,
+                            ),
+                            Text(
+                                'Scientific Name:',
+                                style: TextStyle(
+                                color: fontColor,
+                                fontWeight: fontWeight,
+                                fontSize: fontSize,
+                                ),
+                            ),
+                            Text('\t${jsonDecode(_responseText!)[0]['scientific_name']}\n\n',
+                                textAlign: TextAlign.left,
+                            ),
+                            Text(
+                                'Symptoms:',
+                                style: TextStyle(
+                                color: fontColor,
+                                fontWeight: fontWeight,
+                                fontSize: fontSize,
+                                ),
+                            ),
+                            Text('\t${jsonDecode(_responseText!)[0]['symptoms']}\n\n',
+                                textAlign: TextAlign.left,
+                            ),
+                            Text(
+                                'Treatment:',
+                                style: TextStyle(
+                                color: fontColor,
+                                fontWeight: fontWeight,
+                                fontSize: fontSize,
+                                ),
+                            ),
+                            Text('\t${jsonDecode(_responseText!)[0]['treatment']}\n\n',
+                                textAlign: TextAlign.left,
+                            ),
+                            Text(
+                                'Probability:',
+                                style: TextStyle(
+                                color: fontColor,
+                                fontWeight: fontWeight,
+                                fontSize: fontSize,
+                                ),
+                            ),
+                            Text('\t${(double.parse(jsonDecode(_responseText!)[1]['Probability'])*100).toStringAsFixed(2)} %\n\n',
+                                textAlign: TextAlign.left,
+                            ),
+                          ],
+                        )
+                      : Text(
+                          jsonDecode(_responseText!)['message']+"😁😁😁",
+                          style: const TextStyle(
+                                fontWeight: fontWeight,
+                                fontSize: fontSize,
+                                ),
+                        ),
             ],
           ),
         ),
